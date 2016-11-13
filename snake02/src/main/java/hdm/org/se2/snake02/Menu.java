e21c548647bd107426b1c801ce05477f6208a377
Patrick Dinkelacker <Patrick Dinkelacker@Patrick-Desktop>
2016-11-13 19:56:25 +0100

    Add new Scene and XML Files.

--- snake02/src/main/java/hdm/org/se2/snake02/Menu.java
+++ snake02/src/main/java/hdm/org/se2/snake02/Menu.java
@@ -1,5 +1,42 @@
 package hdm.org.se2.snake02;
 
-public class Menu {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import hdm.org.se2.snake02.Window;
 
+
+public class Menu {
+	
+	Scene menu, game, highscore, settings;
+	
+	@FXML
+	private Button setGameScene;
+	@FXML
+	private Button setHighscoreScene;
+	@FXML
+	private Button setSettingsScene;
+	
+	@FXML
+	void changeScene(ActionEvent event)	{
+		setGameScene.setOnAction(e -> {
+			Window.getStage().setScene(game);
+		});
+		setHighscoreScene.setOnAction(e -> {
+			Window.getStage().setScene(highscore);
+		});
+		setSettingsScene.setOnAction(e -> {
+			Window.getStage().setScene(settings);
+		});
+	}
+	
+	
+	@FXML
+	private void closeProgram()	{
+		System.out.println("Save Game...");
+		//window.close();
+		System.exit(0);
+	}
+	
 }
--- snake02/src/main/java/hdm/org/se2/snake02/Window.java
+++ snake02/src/main/java/hdm/org/se2/snake02/Window.java
@@ -1,37 +1,17 @@
 package hdm.org.se2.snake02;
 
-import javax.management.openmbean.OpenDataException;
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
-import org.omg.CORBA.portable.ApplicationException;
-
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.concurrent.CountDownLatch;
-
-@SuppressWarnings("restriction")
 public class Window extends Application {
 	
 	
-	Stage window;
-	Scene mainMenuScene, settingsScene;
+	Stage windowStage;
+	
+	Scene menu, game, highscore, settings;
 	
 		   
 	public static void main(String[] args) {
@@ -40,71 +20,28 @@ public class Window extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception	{
-		window = primaryStage;
-		window.setTitle("Snake0two");
-
-		// Setting up the labels
-		Label h1 = new Label("Welcome to Snake 0 two");
+		windowStage = primaryStage;
+		windowStage.setTitle("Snake0two");
 		
-		// Setting up the buttons
-		Button play, highscore, settings, quit, btMm;
-		play 		= new Button("Play");
-		highscore 	= new Button("Highscore");
-		settings 	= new Button("Settings");
-		quit 		= new Button("Quit");
-		btMm		= new Button("Back to MainMenu");
-		
-
-		// Event Handler for the play button (New Syntax since Java 1.8)
-		play.setOnAction(e -> {
-				System.out.println("Hello World");
-			});
-		
-		settings.setOnAction(e -> window.setScene(settingsScene));
-
-		btMm.setOnAction(e -> window.setScene(mainMenuScene));
-		
-		// Close functions for the game
-		window.setOnCloseRequest(e -> closeProgram());
-		quit.setOnAction(e -> closeProgram());
-		
-		// Layout 001 - Setup our layout for the menu
-		// The headline on top
-		HBox headline = new HBox();
-		headline.getChildren().add(h1);
-		// The menu at the center
-		VBox menuList = new VBox(20);
-		menuList.getChildren().addAll(play, highscore, settings, quit);
-		
-		// Setup the borderPane and put the headline and menuList inside.
-		GridPane mainMenu = new GridPane();
-		mainMenu.setPadding(new Insets(10,10,10,10));
-		mainMenu.setVgap(8);
-		mainMenu.setHgap(10);
-		
-		mainMenu.setConstraints(headline, 0, 0);
-		mainMenu.setConstraints(menuList, 0, 1);
-		
-		mainMenu.getChildren().addAll(headline, menuList);
-		
-		mainMenuScene = new Scene(mainMenu, 1280, 720);
+		Parent menuSource = FXMLLoader.load(getClass().getResource("/Menu.fxml"));
+		menu = new Scene(menuSource, 1280, 720);
+		Parent gameSource = FXMLLoader.load(getClass().getResource("/Game.fxml"));
+		game = new Scene(gameSource, 1280, 720);
+		Parent highscoreSource = FXMLLoader.load(getClass().getResource("/Highscore.fxml"));
+		highscore = new Scene(highscoreSource, 1280, 720);		
+		Parent settingsSource = FXMLLoader.load(getClass().getResource("/Settings.fxml"));
+		settings = new Scene(settingsSource, 1280, 720);
 		
 		
 		
-		// Layout 002 - Setup our layout for the settings
-		VBox settingsLayout = new VBox(20);
-		settingsLayout.getChildren().addAll(btMm);
-		settingsScene = new Scene(settingsLayout, 1280, 720);
-		
-		
-		window.setScene(mainMenuScene);
-		window.show();
+		windowStage.setScene(menu);
+		windowStage.show();
 		
 	}
-	
-	private void closeProgram()	{
-		System.out.println("Save Game...");
-		window.close();
+
+	public static Object getStage() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
