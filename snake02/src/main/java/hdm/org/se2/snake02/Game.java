92b953d0839925ad311e254a8661279d8fedf811
Patrick Dinkelacker <Patrick Dinkelacker@Patrick-Desktop>
2016-11-17 23:38:24 +0100

    Add a class to create the grid. Implement it to the Game class. Modify the Game.fxml

--- snake02/src/main/java/hdm/org/se2/snake02/Game.java
+++ snake02/src/main/java/hdm/org/se2/snake02/Game.java
@@ -2,13 +2,43 @@ package hdm.org.se2.snake02;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 import hdm.org.se2.snake02.Window;
 
-public class Game {
+public class Game implements Initializable {
+	
+	int gridH = 4, gridW = 4;
+	
+	private int nRows, nCols;
+	
+	
+	
+	//Class containing grid (see below)
+    private GridDisplay gridDisplay;
+	
+	@FXML 
+	private BorderPane GridField;	
+	
+	public void updateGridPane() {		
+		gridDisplay = new GridDisplay(gridH, gridW);
+		
+		GridField.setCenter(gridDisplay.getDisplay());
+    }
 
+	
+	public void initialize(URL location, ResourceBundle resources) {
+		updateGridPane();
+    }
 	
 	
 	/**
---
+++ snake02/src/main/java/hdm/org/se2/snake02/GridDisplay.java
@@ -0,0 +1,65 @@
+package hdm.org.se2.snake02;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class GridDisplay {
+
+	private static final double ELEMENT_SIZE = 100;
+    private static final double GAP = ELEMENT_SIZE / 10;
+	
+	private TilePane tilePane = new TilePane();
+    private Group display = new Group(tilePane);
+    private int nRows;
+    private int nCols;
+	
+    public GridDisplay(int nRows, int nCols) {
+        tilePane.setStyle("-fx-background-color: rgba(255, 215, 0, 0.1);");
+        tilePane.setHgap(GAP);
+        tilePane.setVgap(GAP);
+        setColumns(nCols);
+        setRows(nRows);
+    }
+    
+    public void setColumns(int newColumns) {
+        nCols = newColumns;
+        tilePane.setPrefColumns(nCols);
+        createElements();
+    }
+
+    public void setRows(int newRows) {
+        nRows = newRows;
+        tilePane.setPrefRows(nRows);
+        createElements();
+    }
+
+    public Group getDisplay() {
+        return display;
+    }
+
+    private void createElements() {
+        tilePane.getChildren().clear();
+        for (int i = 0; i < nCols; i++) {
+            for (int j = 0; j < nRows; j++) {
+                tilePane.getChildren().add(createElement());
+            }
+        }
+    }
+
+    private Rectangle createElement() {
+        Rectangle rectangle = new Rectangle(ELEMENT_SIZE, ELEMENT_SIZE);
+        rectangle.setStroke(Color.ORANGE);
+        rectangle.setFill(Color.STEELBLUE);
+
+        return rectangle;
+    }
+    
+}
