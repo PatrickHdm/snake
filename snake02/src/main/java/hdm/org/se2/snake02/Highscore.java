c156540a96758cae95411b926a3df5eb95ae9bfa
Patri <Patri@Patrick-Laptop>
2016-12-01 14:24:20 +0100

    Add highscore class and configure the functions.

--- snake02/src/main/java/hdm/org/se2/snake02/Highscore.java
+++ snake02/src/main/java/hdm/org/se2/snake02/Highscore.java
@@ -1,5 +1,57 @@
 package hdm.org.se2.snake02;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Highscore {
 
+	public void Setter() {
+		try {
+			String name = "\"NTest\"";
+			String score = "\"S2433243242\"";
+			File file = new File("C:/test.csv");
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(name);
+			bw.write(score);
+			bw.close();
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void Getter() {
+		try {
+			FileReader in = new FileReader("C:/test.csv");
+			BufferedReader br = new BufferedReader(in);
+			
+			String temp;
+			String name;
+			String score;
+			while ((temp = br.readLine()) != null) {
+				if(temp.charAt(1)=='N'){
+					name = temp;
+				}
+				if(temp.charAt(1)=='S'){
+					score = temp;
+				}
+			}
+
+			in.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
