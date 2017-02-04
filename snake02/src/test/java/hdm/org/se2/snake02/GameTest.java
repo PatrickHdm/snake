f234dcdd11b3585172a081253f245b2b065d5cb5
lauraschafer <lauraschafer@lauras-macbook-air.fritz.box>
2017-02-04 09:35:29 +0100

    Add GameTest

--- snake02/src/test/java/hdm/org/se2/snake02/GameTest.java
+++ snake02/src/test/java/hdm/org/se2/snake02/GameTest.java
@@ -1,9 +1,58 @@
 package hdm.org.se2.snake02;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class GameTest {
 
+	@Test
+	public void testSceneException () {
 	
+		try {
+			Window.sceneHandler(Window.unknown);
+			fail("Exception");
+		} catch (Exception e) {
+			assertTrue(e instanceof NullPointerException);		
+		}
+	}
+	
+	@Test
+	public void testSettingsDifficulty(){
+	
+		Settings s = new Settings();
+		s.setDifficulty("Medium");
+		Assert.assertEquals(s.getDifficulty(), "Medium");
+		}
+	
+	@Test
+	public void testMode(){
+		Settings se = new Settings();
+		se.setMode("Standard");
+		Assert.assertEquals(se.getMode(), "Standard");
+	}
+	
+	@Test
+	public void negativeModeTest(){
+		Settings neSe = new Settings();
+		try{
+			neSe.setMode("Standi");
+		}catch (Exception e) {
+				assertTrue(e instanceof IOException);
+			}
+		}
+	
+
+
+
 }
+
+
+
+	
+	
+
