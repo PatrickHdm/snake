762721109b98c8383369a7d737669692e913fd83
DJP <DJP@DESKTOP-S7JSHTO>
2017-02-04 17:11:24 +0100

    Highscore Class, Update Controller; fast fertig

--- snake02/src/main/java/hdm/org/se2/snake02/HighscoreController.java
+++ snake02/src/main/java/hdm/org/se2/snake02/HighscoreController.java
@@ -1,13 +1,22 @@
 package hdm.org.se2.snake02;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class HighscoreController {
+public class HighscoreController implements Initializable {
 
 	Logger log = Logger.getLogger(HighscoreController.class.getName());
+	
+	@FXML
+	private BorderPane HighscoreField;
 	
 	@FXML
 	private void toMenu()	{
@@ -16,6 +25,21 @@ public class HighscoreController {
 		} catch (Exception e1) {
 			log.log(Level.SEVERE, "an exception was thrown", e1);
 		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		Highscore highscore = new Highscore();
+		ArrayList<String> scores = highscore.readFromFile();
+		int sScores = scores.size();
+		for (int i = 0; i < sScores; i++) {
+			
+			Label label = new Label(scores.get(i));
+			HighscoreField.setCenter(label);
+		}
+		
+		
 	}
 	
 }
